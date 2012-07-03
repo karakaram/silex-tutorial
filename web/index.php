3a0f1bf34df703e04b8089d0c98e96857e2286d9
karakaram <karakara0141@gmail.com>
2012-07-03 20:11:05 +0900

    hello world

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php'; 
+
+$app = new Silex\Application(); 
+$app['debug'] = true;
+
+$app->get('/hello/{name}', function($name) use($app) { 
+    return 'Hello '.$app->escape($name); 
+}); 
+$app->get('/', function() { 
+    return 'Hello World';
+}); 
+
+$app->run(); 
