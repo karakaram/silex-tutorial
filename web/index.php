f5485817c14e6c0c1a1e4b319fb15e62e30c7e5b
karakaram <karakara0141@gmail.com>
2012-07-03 20:40:00 +0900

    implement twig

--- web/index.php
+++ web/index.php
@@ -4,11 +4,12 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application(); 
 $app['debug'] = true;
 
-$app->get('/hello/{name}', function($name) use($app) { 
-    return 'Hello '.$app->escape($name); 
-}); 
-$app->get('/', function() { 
-    return 'Hello World';
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__ . '/../views',
+));
+
+$app->get('/member/register', function() use($app) { 
+    return $app['twig']->render('member/register.html.twig');
 }); 
 
 $app->run(); 
